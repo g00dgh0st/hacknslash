987e8689e0d8ac20739ceb34855b81f633472f49
Sanjeev Nayak <snayak00+ofrgit@gmail.com>
2020-02-11 17:18:55 -0500

    attempted state management, need to replace default animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum MovementState {
+  Locomotion,
+  Attacking,
+  Rolling
+}
 
 public class PlayerController : MonoBehaviour {
   private Animator anim;
@@ -21,6 +28,7 @@ public class PlayerController : MonoBehaviour {
   private bool isGrounded = true;
   private bool isAttacking = false;
   private Vector3 moveVector;
+  private MovementState movementState;
 
   void Awake() {
     controller = GetComponent<CharacterController>();
@@ -28,48 +36,139 @@ public class PlayerController : MonoBehaviour {
     mainCam = Camera.main;
   }
 
-  void Update() {
-    float h = Input.GetAxis("Horizontal");
-    float v = Input.GetAxis("Vertical");
+  void Start() {
+    movementState = MovementState.Locomotion;
+  }
 
+  void Update() {
     ApplyGravity();
 
-    // if airborne
-
     if (isGrounded) {
-      // grounded locomotion
-      if (isRolling) {
-        HandleRolling();
-      } else {
-        // TODO: state management
-        CheckAttacking();
-        if (Input.GetMouseButtonDown(1)) {
-          StartRoll(GetMoveDirectionByCamera(h, v));
-        } else if (Input.GetMouseButtonDown(0)) {
-          isAttacking = true;
-          anim.SetTrigger("attack");
-        } else if (!isAttacking) {
-          if (Input.GetButtonDown("Jump")) {
-            StartJump();
-          } else {
-            HandleMoving(GetMoveDirectionByCamera(h, v));
-          }
-        }
-
+      switch (movementState) {
+        case MovementState.Locomotion:
+          HandleGroundedControl();
+          break;
+        case MovementState.Rolling:
+          HandleRolling();
+          break;
+        case MovementState.Attacking:
+          HandleAttacking();
+          break;
+        default:
+          // should not happen
+          break;
       }
     } else {
-      // air control
-      HandleTurning(GetMoveDirectionByCamera(h, v), airTurnDampMultiplier);
+      // aerial control
+      HandleAirborneControl();
     }
 
     MakeMove();
   }
 
   void OnAnimatorMove() {
-    if (isAttacking) {
-      // attack animations atm
+    if (movementState == MovementState.Attacking) {
+      // attack animations only atm
       controller.Move(anim.deltaPosition);
     }
+  }
+
+  private void HandleGroundedControl() {
+    Vector3 moveInput = GetInputDirectionByCamera();
+    HandleTurning(moveInput);
+
+    if (Input.GetMouseButtonDown(1)) {
+      StartRoll(moveInput);
+      return;
+    }
+
+    if (Input.GetButtonDown("Jump")) {
+      HandleMoving(moveInput);
+      StartJump(moveInput);
+      return;
+    }
+
+    if (Input.GetMouseButtonDown(0)) {
+      StartAttack(moveInput);
+      return;
+    }
+
+    HandleMoving(moveInput);
+  }
+
+  private void HandleAirborneControl() {
+    HandleTurning(GetInputDirectionByCamera(), airTurnDampMultiplier);
+  }
+
+  private void HandleMoving(Vector3 moveInput) {
+    float inputMagnitude = moveInput.normalized.magnitude;
+
+    moveVector += transform.forward * inputMagnitude * moveSpeed;
+    anim.SetFloat("speed", Mathf.Lerp(anim.GetFloat("speed"), inputMagnitude, locomotionDampen));
+  }
+
+  private void HandleTurning(Vector3 moveInput, float multiplier = 1f) {
+    if (moveInput.magnitude == 0f) return;
+
+    Quaternion targetDirection = Quaternion.LookRotation(moveInput);
+    transform.rotation = Quaternion.Lerp(transform.rotation, targetDirection, turnDamped * multiplier);
+  }
+
+  private void HandleRolling() {
+    moveVector += transform.forward * rollSpeed;
+    anim.ResetTrigger("roll");
+  }
+
+  private void HandleAttacking() {
+    CheckAttacking();
+
+    Vector3 moveInput = GetInputDirectionByCamera();
+    if (Input.GetMouseButtonDown(0)) {
+      ContinueAttack(moveInput);
+      return;
+    }
+  }
+
+  // NOTE: hacky fix
+  private float startAttackTime;
+  private void StartAttack(Vector3 moveInput) {
+    movementState = MovementState.Attacking;
+    startAttackTime = Time.time;
+    anim.SetTrigger("attack");
+  }
+
+  private void ContinueAttack(Vector3 moveInput) {
+    anim.SetTrigger("attack");
+  }
+
+  private void CheckAttacking() {
+    AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+    AnimatorTransitionInfo transInfo = anim.GetAnimatorTransitionInfo(0);
+    if (Time.time - startAttackTime > 0.3f && !info.IsTag("Attacking") && !transInfo.IsName("Attacking")) {
+      Debug.Log("FDSFSFSFFDSFDSF");
+      movementState = MovementState.Locomotion;
+      anim.ResetTrigger("attack");
+    }
+  }
+
+  void StartRoll(Vector3 rollDirection) {
+    anim.SetTrigger("roll");
+  }
+
+  // Animation Event
+  public void Rolling(int rolling) {
+    movementState = (rolling == 1 ? MovementState.Rolling : MovementState.Locomotion);
+  }
+
+  void StartJump(Vector3 moveInput) {
+    // TODO: use move vector to affect jump
+    anim.SetTrigger("jump");
+    moveVector += (Vector3.up * jumpSpeed);
+  }
+
+  // Apply all movement at once, so there is only one Move call
+  void MakeMove() {
+    controller.Move(moveVector * Time.deltaTime);
   }
 
   private bool ApplyGravity() {
@@ -90,49 +189,10 @@ public class PlayerController : MonoBehaviour {
     return false;
   }
 
-  // Apply all movement at once, so there is only one Move call
-  void MakeMove() {
-    controller.Move(moveVector * Time.deltaTime);
-  }
+  private Vector3 GetInputDirectionByCamera() {
+    float horizontalAxis = Input.GetAxis("Horizontal");
+    float verticalAxis = Input.GetAxis("Vertical");
 
-  private void HandleRolling() {
-    moveVector += transform.forward * rollSpeed;
-    anim.ResetTrigger("roll");
-  }
-
-  private void HandleMoving(Vector3 moveInput) {
-    float inputMagnitude = moveInput.normalized.magnitude;
-
-    HandleTurning(moveInput);
-
-    moveVector += transform.forward * inputMagnitude * moveSpeed;
-    anim.SetFloat("speed", Mathf.Lerp(anim.GetFloat("speed"), inputMagnitude, locomotionDampen));
-  }
-
-  private void HandleTurning(Vector3 moveInput, float multiplier = 1f) {
-    if (moveInput.magnitude == 0f) return;
-
-    Quaternion targetDirection = Quaternion.LookRotation(moveInput);
-    transform.rotation = Quaternion.Lerp(transform.rotation, targetDirection, turnDamped * multiplier);
-  }
-
-  private void CheckAttacking() {
-    AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
-    isAttacking = info.IsTag("Combat");
-    if (!isAttacking) anim.ResetTrigger("attack");
-  }
-
-  void StartJump() {
-    anim.SetTrigger("jump");
-    moveVector += Vector3.up * jumpSpeed;
-  }
-
-  void StartRoll(Vector3 rollDirection) {
-    anim.SetTrigger("roll");
-    HandleTurning(rollDirection, 100f);
-  }
-
-  private Vector3 GetMoveDirectionByCamera(float horizontalAxis, float verticalAxis) {
     //camera forward and right vectors:
     var forward = mainCam.transform.forward;
     var right = mainCam.transform.right;
@@ -145,10 +205,5 @@ public class PlayerController : MonoBehaviour {
 
     //this is the direction in the world space we want to move:
     return forward * verticalAxis + right * horizontalAxis;
-  }
-
-  // Animation Event
-  public void Rolling(int rolling) {
-    isRolling = (rolling == 1 ? true : false);
   }
 }
